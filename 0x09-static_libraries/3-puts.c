4fd7f941922867ba5f51fe756e37c2f8994c4ba5
sethu <sethuseti08@gmail.com>
2023-03-10 19:17:51 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/*
+ *
+ * main - Entry point
+ *
+ * void _puts(char *tr)
+ * Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
